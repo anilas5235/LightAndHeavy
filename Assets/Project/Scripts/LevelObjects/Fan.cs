6e48141368143e6c3226ba59879332bea747145d
mazefighter <max.hannuschka04@gmail.com>
2024-04-15 12:11:27 +0200

    Niki

---
+++ Assets/Project/Scripts/LevelObjects/Fan.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Project.Scripts.LevelObjects;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D boxCollider2D;
+    [SerializeField] private AreaEffector2D areaEffector2D;
+
+    [SerializeField] private BoolInteractable[] activator;
+
+    private void OnEnable()
+    {
+        foreach (BoolInteractable boolInteractable in activator)
+        {
+            boolInteractable.onStateChange.AddListener(_stateChange);
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (BoolInteractable boolInteractable in activator)
+        {
+            boolInteractable.onStateChange.RemoveListener(_stateChange);
+        }
+    }
+
+    private void Awake()
+    {
+        _stateChange(false);
+    }
+
+    private void _stateChange(bool arg0)
+    {
+        boxCollider2D.enabled = arg0;
+        areaEffector2D.enabled = arg0;
+    }
+
+    
+
+    
+
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
