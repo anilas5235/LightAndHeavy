d50001b4e0af2ce657683dbc49c2f8d7464ce464
anilas5235 <niklas-borchers@outlook.de>
2024-04-15 12:14:30 +0200

    platfroms

--- Assets/ControllerPlugin/Scripts/BasicCharacterController2D.cs
+++ Assets/ControllerPlugin/Scripts/BasicCharacterController2D.cs
@@ -3,80 +3,108 @@ using AttributesLibrary.DrawIfAttribute;
 using Project.Scripts.Attributes;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace ControllerPlugin.Scripts
 {
-    [RequireComponent(typeof(Rigidbody2D),typeof(BoxCollider2D))]
+    [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
     public class BasicCharacterController2D : MonoBehaviour
     {
         [SerializeField] protected LayerMask groundLayers;
-        
-        [Header("Speed Settings"),SerializeField, Range(0f, 100f)] protected float maxSpeed = 4f;
+
+        [Header("Speed Settings"), SerializeField, Range(0f, 100f)]
+        protected float maxSpeed = 4f;
+
         [SerializeField, Range(0f, 100f)] protected float maxFallSpeed = 6f;
         [SerializeField, Range(0f, 100f)] protected float maxAcceleration = 35f;
         [SerializeField, Range(0f, 100f)] protected float maxAirAcceleration = 20f;
+        [SerializeField, Range(0.01f, 10f)] protected float gravityScale = 1f;
 
-        [Header("Jump Settings"),SerializeField] protected bool canJump = true;
-        [DrawIf("canJump", true, ComparisonType.Equals),SerializeField, Range(0f, 10f)] private float jumpHeight = 3f;
-        [DrawIf("canJump", true, ComparisonType.Equals),SerializeField, Range(0, 5)] private int maxAirJumps = 0;
-        [DrawIf("canJump", true, ComparisonType.Equals),SerializeField, Range(0f, 5f)] protected float downwardMovementMultiplier = 3f;
-        [DrawIf("canJump", true, ComparisonType.Equals),SerializeField, Range(0f, 5f)] protected float upwardMovementMultiplier = 1.7f;
+        [Header("Jump Settings"), SerializeField]
+        protected bool canJump = true;
+
+        [DrawIf("canJump", true, ComparisonType.Equals), SerializeField, Range(0f, 10f)]
+        private float maxJumpHeight = 3f;
         
-        [Header("Info"),SerializeField, ReadOnly] private Vector2 velocity;
+        [DrawIf("canJump", true, ComparisonType.Equals), SerializeField, Range(0f, 10f)]
+        private float minJumpHeight = .25f;
+        
+        [DrawIf("canJump", true, ComparisonType.Equals), SerializeField, Range(0f, 3f)]
+        private float timeTillApex = 1f;
+
+        [DrawIf("canJump", true, ComparisonType.Equals), SerializeField, Range(0, 5)]
+        private int maxAirJumps;
+
+        [Header("Info"), SerializeField, ReadOnly]
+        private Vector2 velocity;
+
         [SerializeField, ReadOnly] private Vector2Int input;
         [SerializeField, ReadOnly] private bool onGround;
         [SerializeField, ReadOnly] private bool isJumping;
 
-        private Rigidbody2D _rigidbody2D;
+        [SerializeField, ReadOnly] private float maxJumpVelocity;
+        [SerializeField, ReadOnly] private float minJumpVelocity;
+        [SerializeField, ReadOnly] private float defaultGravityScale;
+        
+        private Rigidbody2D _rigidBody2D;
         private BoxCollider2D _boxCollider2D;
-        private float _defaultGravityScale = 1f;
+        
         private bool _jumpInput;
         private bool _jumpCanceled;
+        
         private int _airJumps;
         private bool _doneGroundCheck = true;
+        private Coroutine groundCoroutine;
 
         protected virtual void Awake()
         {
-            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _rigidBody2D = GetComponent<Rigidbody2D>();
             _boxCollider2D = GetComponent<BoxCollider2D>();
+            CalculateJumpValues();
+        }
+
+        private void CalculateJumpValues()
+        {
+            _rigidBody2D.gravityScale = gravityScale;
+            maxJumpVelocity = gravityScale* 2*maxJumpHeight * timeTillApex;
+            minJumpVelocity = Mathf.Sqrt(2 *gravityScale * minJumpHeight);
         }
 
         protected void FixedUpdate()
         {
-            velocity = _rigidbody2D.velocity;
-            if(_doneGroundCheck) onGround = CheckIfOnGround();
+            velocity = _rigidBody2D.velocity;
+            if (_doneGroundCheck) onGround = CheckIfOnGround();
             if (isJumping && onGround) isJumping = false;
 
-            velocity.x = CalculateNewXVelocityFromInput(input,velocity);
+            velocity.x = CalculateNewXVelocityFromInput(input, velocity);
 
-            if (canJump&&_jumpInput)
+            if (canJump && _jumpInput)
             {
                 _jumpInput = false;
                 if (onGround)
                 {
                     _doneGroundCheck = false;
-                    StartCoroutine(ActivateGroundDetectAfter(.2f));
+                    if(groundCoroutine != null) StopCoroutine(groundCoroutine);
+                    groundCoroutine = StartCoroutine(ActivateGroundDetectAfter(.2f));
                     isJumping = true;
                     _airJumps = 0;
                     HandleJump(ref velocity);
                     onGround = false;
                 }
-                else if(isJumping && _airJumps < maxAirJumps)
+                else if (isJumping && _airJumps < maxAirJumps)
                 {
                     isJumping = true;
-                    HandleJump(ref velocity); 
+                    HandleJump(ref velocity);
                 }
             }
-
-            /*if (isJumping && jumpCanceled && velocity.y >0)
-            {
-                jumpCanceled = false;
-                JumpCanceled(ref velocity);
-            }*/
-                
-            UpdateGravityScale();
+            else if (isJumping && _jumpCanceled)
+            { 
+                HandleJumpCanceled(ref velocity);
+                _jumpCanceled = false;
+            }
+            
             LastMinuteVelocityConstrains();
-            _rigidbody2D.velocity = velocity;
+            _rigidBody2D.velocity = velocity;
         }
 
         protected virtual float CalculateNewXVelocityFromInput(Vector2Int inputVector, Vector2 currentVelocity)
@@ -87,20 +115,14 @@ namespace ControllerPlugin.Scripts
             return Mathf.MoveTowards(currentVelocity.x, desiredVelocity.x, maxSpeedChange);
         }
 
-        protected virtual void UpdateGravityScale()
-        {
-            _rigidbody2D.gravityScale = _rigidbody2D.velocity.y switch
-            {
-                > 0 => upwardMovementMultiplier,
-                < 0 => downwardMovementMultiplier,
-                0 => _defaultGravityScale,
-                _ => _rigidbody2D.gravityScale
-            };
-        }
-
         protected virtual void HandleJump(ref Vector2 currentVelocity)
         {
-            currentVelocity.y += jumpHeight;
+            currentVelocity.y = maxJumpVelocity;
+        }
+
+        protected virtual void HandleJumpCanceled(ref Vector2 currentVelocity)
+        {
+            if (currentVelocity.y > minJumpVelocity) currentVelocity.y = minJumpVelocity;
         }
 
         protected virtual void LastMinuteVelocityConstrains()
@@ -108,46 +130,36 @@ namespace ControllerPlugin.Scripts
             if (onGround) velocity.y = 0;
             else if (velocity.y < -maxFallSpeed) velocity.y = -maxFallSpeed;
         }
+
         protected virtual bool CheckIfOnGround()
         {
             var results = new RaycastHit2D[10];
             var size = Physics2D.BoxCastNonAlloc(transform.position + (Vector3)_boxCollider2D.offset,
-                _boxCollider2D.size, 0, Vector2.down, results, .05f,groundLayers);
+                _boxCollider2D.size, 0, Vector2.down, results, .05f, groundLayers);
 
             for (var i = 0; i < size; i++)
             {
                 var rayCastHit2D = results[i];
-                if(rayCastHit2D.collider.isTrigger||rayCastHit2D.transform.gameObject == gameObject) continue;
+                if (rayCastHit2D.collider.isTrigger || rayCastHit2D.transform.gameObject == gameObject) continue;
                 return true;
             }
 
             return false;
         }
 
-        protected virtual void JumpCanceled(ref Vector2 currentVelocity)
-        {
-            currentVelocity.y = 0;
-        }
-
-        IEnumerator ActivateGroundDetectAfter(float sec)
+        private IEnumerator ActivateGroundDetectAfter(float sec)
         {
             yield return new WaitForSeconds(sec);
             _doneGroundCheck = true;
+            groundCoroutine = null;
         }
 
         public virtual void JumpInput(InputAction.CallbackContext context)
         {
-            if (context.started)
-            {
-                _jumpInput = true;
-            }
-
-            if (context.canceled)
-            {
-                _jumpCanceled = true;
-            }
+            if (!_jumpInput && context.started) _jumpInput = true;
+            if (!_jumpCanceled && context.canceled) _jumpCanceled = true;
         }
-        
+
         public virtual void HorizontalInput(InputAction.CallbackContext context)
         {
             input.x = Mathf.RoundToInt(context.ReadValue<float>());
--- Assets/Project/Scripts/LevelObjects/MovablePlatform.cs
+++ Assets/Project/Scripts/LevelObjects/MovablePlatform.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -5,31 +8,86 @@ namespace Project.Scripts.LevelObjects
 {
     public class MovablePlatform : BoolInteractable
     {
-        [SerializeField] private Vector2 lowerOffsetPoint;
-        [SerializeField] private Vector2 upperOffsetPoint;
-
-        private Vector2 _startPosition;
+        [SerializeField] private Transform[] worldPoints;
 
         [SerializeField] private BoolInteractable[] triggers;
-
-        private void Awake()
-        {
-           _startPosition = transform.position;
-        }
-
+        [SerializeField] private float speed = 5;
+       
+        private Vector3 targetPoint;
+        private int currentIndex;
+        private Coroutine moveRoutine;
+        private List<GameObject> children = new List<GameObject>();
+     
         private void OnEnable()
         {
             foreach (var trigger in triggers)trigger.onStateChange.AddListener(CheckTriggers);
         }
 
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.attachedRigidbody)
+            {
+                children.Add(other.gameObject);
+                other.transform.parent = transform;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.attachedRigidbody)
+            {
+                children.Remove(other.gameObject);
+                other.transform.parent = null;
+            }
+        }
+
         private void OnDisable()
         {
+            foreach (var child in children)
+            {
+                child.transform.parent = null;
+            }
             foreach (var trigger in triggers)trigger.onStateChange.RemoveListener(CheckTriggers);
         }
 
         private void CheckTriggers(bool arg0)
         {
             State = triggers.Any(trigger => trigger.State);
+            if(moveRoutine != null)StopCoroutine(moveRoutine);
+            StartCoroutine(PlatformMove(State));
+        }
+        private IEnumerator PlatformMove(bool rise)
+        {
+            targetPoint = rise ? worldPoints[0].position : worldPoints[^1].position;
+            var done=false;
+            do
+            {
+                var vec = targetPoint - transform.position;
+                if (vec.magnitude > .1f)
+                {
+                    transform.Translate(vec.normalized * (Time.deltaTime * speed));
+                }
+                else
+                {
+                    currentIndex += rise ? 1 : -1;
+                    if (currentIndex < 0)
+                    {
+                        currentIndex = 0;
+                        done = true;
+                    }
+                    else if (currentIndex >= worldPoints.Length)
+                    {
+                        currentIndex = worldPoints.Length - 1;
+                        done = true;
+                    }
+
+                    targetPoint = worldPoints[currentIndex].position;
+                }
+
+                yield return null;
+            } while (!done);
+
+            moveRoutine = null;
         }
     }
 }
