355c90cc76534f41df49b28b0934ee29fe5b43d0
mazefighter <max.hannuschka04@gmail.com>
2024-04-22 10:04:42 +0200

    Smol

---
+++ Assets/Project/Scripts/UI/LevelState.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Project.Scripts.LevelObjects;
+using UnityEngine;
+
+public class LevelState : MonoBehaviour
+{
+    [SerializeField] private FinishDoor[] doors = new FinishDoor[2];
+    [SerializeField] private UIPanelControll winControll;
+    void Start()
+    {
+        foreach (FinishDoor finishDoor in doors)
+        {
+            finishDoor.onDoorOpened.AddListener(DoorOpenedCheck);
+        }
+    }
+
+    private void DoorOpenedCheck()
+    {
+        bool allOpen = true;
+        foreach (FinishDoor finishDoor in doors)
+        {
+            if (finishDoor.Open == false)
+            {
+                allOpen = false;
+            }
+        }
+
+        if (allOpen)
+        {
+            winControll.gameObject.SetActive(true);
+            winControll.Stars = 3;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
