ee1c8c9df11571e68a4afe0bb16d93a8a136bcb1
mazefighter <max.hannuschka04@gmail.com>
2024-04-23 18:02:54 +0200

    AudioIstShit

--- Assets/Project/Scripts/Player/AnimationController.cs
+++ Assets/Project/Scripts/Player/AnimationController.cs
@@ -1,6 +1,7 @@
 using System;
 using ControllerPlugin.Scripts;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Project.Scripts.Player
 {
@@ -8,11 +9,15 @@ namespace Project.Scripts.Player
     {
         [SerializeField] private AdvancedCharacterController2D advancedCharacterController2D;
         private Animator animator;
+        private AudioSource _audioSource;
+        [SerializeField] private AudioClip[] dashSounds;
+        [SerializeField] private AudioClip[] jumpSounds;
         private static readonly int Run = Animator.StringToHash("Run");
 
         private void Awake()
         {
             animator = GetComponent<Animator>();
+            _audioSource = GetComponent<AudioSource>();
         }
 
         private void OnEnable()
@@ -28,6 +33,19 @@ namespace Project.Scripts.Player
         private void StateChanged(CharacterActionState old, CharacterActionState newState)
         {
             animator.SetBool(Run,newState == CharacterActionState.Walking);
+            if (newState == CharacterActionState.Dashing && gameObject.layer == LayerMask.NameToLayer("Heavy"))
+            {
+                int rndm = Random.Range(0, dashSounds.Length);
+                _audioSource.clip = dashSounds[rndm];
+                _audioSource.Play();
+            }
+
+            if (newState == CharacterActionState.Jumping && gameObject.layer == LayerMask.NameToLayer("Light"))
+            {
+                int rndm = Random.Range(0, jumpSounds.Length);
+                _audioSource.clip = jumpSounds[rndm];
+                _audioSource.Play();
+            }
         }
     }
 }
